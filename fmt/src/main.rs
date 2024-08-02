9bd8131fd6b12061e4eb01939a581888b1409c35
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2024-08-02 11:55:30 +0900

    fmt update

--- fmt/src/main.rs
+++ fmt/src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     println!("{} days", 31);
 
     println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    println!("{subject} {verb} {object}",
+            object="the lazy dog",
+            subject="the quick brown fox",
+            verb="jumps over");
 }
